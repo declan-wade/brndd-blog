72802bdbc2e9145cb4e0e236d6dfdec3cd510e74
declan-enercloud <declan@enercloud.io>
2024-06-05 15:11:06 +0800

    Setup

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,20 @@
 import '../styles/globals.css';
-
+import styles from './index.module.css';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Notion Next.js blog',
-  description: 'Notion Next.js blog',
+  title: 'BRNDD BLOG',
+  description: 'Hear more about the BRNDD community and what we\'re working on.'
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <header className={styles.logos}>
+          <p className="font-logo">BRNDD  BLOG</p>
+          </header>
       <body className={inter.className}>
         {children}
       </body>
